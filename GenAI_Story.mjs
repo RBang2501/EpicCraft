cb218bb3f144960225a6c8e24b75a2638d702aca
Shreyansh Rai <shreyanshrajeevarai@gmail.com>
2024-01-26 16:13:48 +0530

    Readme and model

--- GenAI_Story.mjs
+++ GenAI_Story.mjs
@@ -9,8 +9,8 @@ function getLoadModelLink(){
     return "https://television-doing-pharmacies-sucking.trycloudflare.com/v1/internal/model/load";
 }
 
-const baseinstruct = "Write a Chapters of story about The Masquerade of Masks . start the story with input . Chapter length 150 words only";
-const baseinput = 'Beneath the glittering chandeliers and swirling waltzes, secrets danced behind gilded masks. Infiltrating the opulent ball, I donned a visage of my own, determined to expose the web of deceit and uncover the identity of the phantom lurking in the shadows.';
+const baseinstruct = "Write a Chapters of story about The Last Librarian of Alexandria start the story with input . Chapter length 150 words only";
+const baseinput = 'Dust motes danced in the sunbeams piercing through the vaulted ceiling. As the sole custodian of the legendary Library of Alexandria, I navigated towering shelves laden with scrolls whispering tales of forgotten empires and hidden knowledge. But shadows lurk in the labyrinthine stacks, and a forbidden tome beckons with promises of power and peril. (Image of the Last Librarian of Alexandria surrounded by ancient scrolls)';
 
 
 
@@ -209,23 +209,23 @@ async function Publish(title, body, id="")
     const id1 = await Publish(title, Twist1,id0.data.hashId);
     ResponseList.push(id1);
 
-    const Twist2 = await getContinuation("given previously written chapter passed as input, Write NEXT 1 Chapter of story within 150 words ONLY", RootTwist + Twist1);
-    title = await getTitle(Twist2);
-    console.log(`\n\n\n${title}\n${Twist2}`);
-    const id2 = await Publish(title, Twist2,id1.data.hashId);
-    ResponseList.push(id2);
+    // const Twist2 = await getContinuation("given previously written chapter passed as input, Write NEXT 1 Chapter of story within 150 words ONLY", RootTwist + Twist1);
+    // title = await getTitle(Twist2);
+    // console.log(`\n\n\n${title}\n${Twist2}`);
+    // const id2 = await Publish(title, Twist2,id1.data.hashId);
+    // ResponseList.push(id2);
 
-    const Twist3 = await getContinuation("given previously written chapter passed as input, Write NEXT 1 Chapter of story within 150 words ONLY", RootTwist + Twist1 + Twist2);
-    title = await getTitle(Twist3);
-    console.log(`\n\n\n${title}\n${Twist3}`);
-    const id3 = await Publish(title, Twist3,id2.data.hashId);
-    ResponseList.push(id3);
+    // const Twist3 = await getContinuation("given previously written chapter passed as input, Write NEXT 1 Chapter of story within 150 words ONLY", RootTwist + Twist1 + Twist2);
+    // title = await getTitle(Twist3);
+    // console.log(`\n\n\n${title}\n${Twist3}`);
+    // const id3 = await Publish(title, Twist3,id2.data.hashId);
+    // ResponseList.push(id3);
 
     const leafTwist = "Thrilling good ending, feel good, entertaining.";
     var Twist4 = await getContinuation("given previously written story passed as input, write an ending to the story within 150 words ONLY", RootTwist, leafTwist);
     title = await getTitle(Twist4)
     console.log(`\n\n\n${title}\n${Twist4}`);
-    const id4 = await Publish(title, Twist4,id3.data.hashId);
+    const id4 = await Publish(title, Twist4,id1.data.hashId);
     ResponseList.push(id4);
     
     finalise(ResponseList)
